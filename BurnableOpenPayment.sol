2bc510eab37bd971987abd37a06746c0ed7b052b
coinop-logan <coinop.logan@gmail.com>
2017-05-01 17:17:55 +0200

    Added some features discussed in https://www.reddit.com/r/DappLab/comments/65bb9a/bop_v2_ideas/
    - Added a default timeout which assures the recipient he won't fall victim to simple laziness on the payer's fault.
    - Added a recover() function that can be called by payer in the Open state; suicides contract and returns funds.
    - Added states (Open, Committed, Exhausted) and associated checks.
    - payerString now specified in constructor.
    - Added events.
    - Added internalBurn/internalRelease (private but doesn't check msg.sender), which burn/release uses (after checking sender) and callDefaultAction can call.

--- BurnableOpenPayment.sol
+++ BurnableOpenPayment.sol
@@ -1,8 +1,8 @@
 //A BurnableOpenPaymet is instantiated with a specified payer and a commitThreshold.
-//The recipient not set when the contract is instantiated.
+//The recipient is not set when the contract is instantiated.
 
 //The constructor is payable, so the contract can be instantiated with initial funds.
-//Anyone can contribute to the payment at any time (the () function is payable).
+//Only the payer can fund the Payment after instantiation.
 
 //All behavior of the contract is directed by the payer, but
 //the payer can never directly recover the payment unless he becomes the recipient.
@@ -12,85 +12,139 @@
 
 //The payer can at any time choose to burn or release to the recipient any amount of funds.
 
-pragma solidity ^0.4.1;
+pragma solidity ^0.4.10;
 
 contract BurnableOpenPayment {
     address public payer;
     address public recipient;
-    address public burnAddress = 0xdead;
+    address constant burnAddress = 0x0;
+    
     string public payerString;
     string public recipientString;
+    
     uint public commitThreshold;
     
-    modifier onlyPayer() {
-        if (msg.sender != payer) throw;
-        _;
-    }
+    enum DefaultAction {Burn, Release, None}
+    DefaultAction public defaultAction;
+    uint public defaultTimeoutLength;
+    uint public defaultTriggerTime;
     
-    modifier onlyRecipient() {
-        if (msg.sender != recipient) throw;
-        _;
-    }
+    enum State {Open, Committed, Expended}
+    State public state;
     
-    modifier onlyWithRecipient() {
-        if (recipient == address(0x0)) throw;
-        _;
-    }
+    modifier inState(State s) { if (s != state) throw; _; }
+    modifier onlyPayer() { if (msg.sender != payer) throw; _; }
+    modifier onlyRecipient() { if (msg.sender != recipient) throw; _; }
+    modifier onlyPayerOrRecipient() { if ((msg.sender != payer) && (msg.sender != recipient)) throw; _; }
     
-    modifier onlyWithoutRecipient() {
-        if (recipient != address(0x0)) throw;
-        _;
-    }
+    event FundsAdded(uint amount);
+    event PayerStringUpdated(string newPayerString);
+    event RecipientStringUpdated(string newRecipientString);
+    event FundsRecovered();
+    event Committed(address recipient);
+    event FundsBurned(uint amount);
+    event FundsReleased(uint amount);
+    event Expended();
+    event Unexpended();
+    event DefaultTimeoutDelayed();
+    event DefaultActionCalled();
     
-    function () payable {}
-    
-    function BurnableOpenPayment(address _payer, uint _commitThreshold)
+    function BurnableOpenPayment(address _payer, string _payerString, uint _commitThreshold, DefaultAction _defaultAction, uint _defaultTimeoutLength)
     public
     payable {
+        state = State.Open;
         payer = _payer;
+        payerString = _payerString;
+        PayerStringUpdated(payerString);
         commitThreshold = _commitThreshold;
+        defaultAction = _defaultAction;
+        defaultTimeoutLength = _defaultTimeoutLength;
     }
     
-    function getPayer()
-    public returns (address) { return payer; }
+    function addFunds()
+    public
+    onlyPayer()
+    payable {
+        if (msg.value == 0) throw;
+        FundsAdded(msg.value);
+        if (state == State.Expended) {
+            state = State.Committed;
+            Unexpended();
+        }
+    }
     
-    function getRecipient()
-    public returns (address) { return recipient; }
-    
-    function getCommitThreshold()
-    public returns (uint) { return commitThreshold; }
-    
-    function getPayerString()
-    public returns (string) { return payerString; }
-    
-    function getRecipientString()
-    public returns (string) { return recipientString; }
+    function recoverFunds()
+    public
+    onlyPayer()
+    inState(State.Open)
+    {
+        FundsRecovered();
+        selfdestruct(payer);
+    }
     
     function commit()
     public
-    onlyWithoutRecipient()
+    inState(State.Open)
     payable
     {
         if (msg.value < commitThreshold) throw;
         recipient = msg.sender;
+        state = State.Committed;
+        Committed(recipient);
+        
+        if (defaultAction != DefaultAction.None) {
+            defaultTriggerTime = now + defaultTimeoutLength;
+        }
+    }
+    
+    function internalBurn(uint amount)
+    private
+    inState(State.Committed)
+    returns (bool)
+    {
+        bool success = burnAddress.send(amount);
+        if (success) {
+            FundsBurned(amount);
+        }
+        if (this.balance == 0) {
+            state = State.Expended;
+            Expended();
+        }
+        return success;
     }
     
     function burn(uint amount)
     public
+    inState(State.Committed)
     onlyPayer()
-    onlyWithRecipient()
     returns (bool)
     {
-        return burnAddress.send(amount);
+        return internalBurn(amount);
+    }
+    
+    function internalRelease(uint amount)
+    private
+    inState(State.Committed)
+    returns (bool)
+    {
+        bool success = recipient.send(amount);
+        if (success) {
+            FundsReleased(amount);
+        }
+        if (this.balance == 0) {
+            state = State.Expended;
+            Expended();
+        }
+        return success;
     }
     
     function release(uint amount)
     public
+    inState(State.Committed)
     onlyPayer()
-    onlyWithRecipient()
     returns (bool)
     {
-        return recipient.send(amount);
+        return internalRelease(amount);
     }
     
     function setPayerString(string _string)
@@ -98,6 +152,7 @@ contract BurnableOpenPayment {
     onlyPayer()
     {
         payerString = _string;
+        PayerStringUpdated(payerString);
     }
     
     function setRecipientString(string _string)
@@ -105,15 +160,48 @@ contract BurnableOpenPayment {
     onlyRecipient()
     {
         recipientString = _string;
+        RecipientStringUpdated(recipientString);
+    }
+    
+    function delayDefaultAction()
+    public
+    onlyPayerOrRecipient()
+    inState(State.Committed)
+    {
+        if (defaultAction == DefaultAction.None) throw;
+        
+        DefaultTimeoutDelayed();
+        defaultTriggerTime = now + defaultTimeoutLength;
+    }
+    
+    function callDefaultAction()
+    public
+    onlyPayerOrRecipient()
+    inState(State.Committed)
+    {
+        if (defaultAction == DefaultAction.None) throw;
+        if (now < defaultTriggerTime) throw;
+        
+        DefaultActionCalled();
+        if (defaultAction == DefaultAction.Burn) {
+            internalBurn(this.balance);
+        }
+        else if (defaultAction == DefaultAction.Release) {
+            internalRelease(this.balance);
+        }
     }
 }
 
 contract BurnableOpenPaymentFactory {
-    function newBurnableOpenPayment(address payer, uint commitThreshold)
+    event NewBOP(address newBOPAddress);
+    
+    function newBurnableOpenPayment(address payer, string payerString, uint commitThreshold, BurnableOpenPayment.DefaultAction defaultAction, uint defaultTimeoutLength)
     public
     payable
     returns (address) {
         //pass along any ether to the constructor
-        return (new BurnableOpenPayment).value(msg.value)(payer, commitThreshold);
+        address newBOPAddr = (new BurnableOpenPayment).value(msg.value)(payer, payerString, commitThreshold, defaultAction, defaultTimeoutLength);
+        NewBOP(newBOPAddr);
+        return newBOPAddr;
     }
 }
